1bf40b219c3d295a21b8a6ea79188fde453994c2
Robin A <robinand@live.com>
2023-03-23 10:49:40 +0000

    initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,41 @@
 
 // Vanilla JavaScript Function
 
+//function addTwoNumbers(a,b) {
+//    return a + b;
+//}
+//
+//let sum = addTwoNumbers(5, 9);
+//console.log(sum);
+
 // Arrow Function With Parameters
 
+const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+
+let sum = addTwoNumbers(5, 9);
+console.log(sum);
+
 // Single Line Arrow Function With Parameters
+//const addTwoNumbers2 = (a, b) => (a + b); //Also Valid
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(4, 7)
+console.log(sum2);
 
 // Implicit Returns
+const speak = message => console.log(message);
+speak("Welcome!");
+
+const sayHello = () => console.log("Hello");
+sayHello();
 
 // Returning Multiple Lines
+
+const returnMultiLines = () => (
+    `<p>
+     This is a multiline string!
+    </p>`
+)
+
+console.log(returnMultiLines());
